2be4e7e29b1f4f396d8607b1c5fdc05268f921d5
Evan Phillips <98239647+EvanPhillips34@users.noreply.github.com>
2026-01-17 14:09:15 -0600

    fixed ts

--- src/code/HiddenLayer.java
+++ src/code/HiddenLayer.java
@@ -12,7 +12,6 @@ public class HiddenLayer {
     public int nCount;
     public int iCount;
 
-    public static ArrayList<HiddenLayer> layers = new ArrayList<>();
     public static ArrayList<HiddenLayer> originals = new ArrayList<>();
 
     public HiddenLayer(int neuronCount, int inputCount) {
@@ -33,36 +32,18 @@ public class HiddenLayer {
             biases[i] = (float) Math.random();
         }
         originals.add(this);
-        layers.add(new HiddenLayer(this, false));
 
     }
 
-
-    public HiddenLayer(HiddenLayer layer, boolean update) {
-        this.nCount = layer.nCount;
-        this.iCount = layer.iCount;
-        this.weights = layer.weights.clone();
-        this.biases = layer.biases.clone();
-        if(update) {
-            this.outputs = layer.outputs.clone();
-            this.inputs = layer.inputs.clone();
+    public ArrayList<HiddenLayer> createLayers(int neuronCount, int layerCount) {
+        ArrayList<HiddenLayer> list = new ArrayList<>();
+        for(int i = 0; i < layerCount; i++) {
+            list.add(new HiddenLayer(neuronCount, neuronCount));
         }
-
+        return list;
     }
 
-    
 
-    public static void syncData() {
-        ArrayList<HiddenLayer> bufferList = new ArrayList<>();
-        for(HiddenLayer layer : originals) {
-            bufferList.add(new HiddenLayer(layer, true));
-        }
-        layers.clear();
-        System.out.println("This is the buffer list length: " + bufferList.size());
-        layers = bufferList;
-        System.out.println("This is the updated and synced list length: " + layers.size());
-
-    }
 
     public void forward(float[][] inputs) throws Exception {
         this.inputs = inputs;
